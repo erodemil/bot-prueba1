8b1cb567920228615a52e812907809408094ac88
erodemil <djpromto@gmail.com>
2025-10-19 14:28:49 -0400

    Refactor bot initialization to main guard

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
-TOKEN = 8273893147:AAHI7WEUSgdgm9AVq5aMhgk24dKeJQ8KHM8
+TOKEN = "8273893147:AAHI7WEUSgdgm9AVq5aMhgk24dKeJQ8KHM8"
 
 async def responder(update: Update, context: ContextTypes.DEFAULT_TYPE):
     texto = update.message.text.lower()
@@ -11,8 +11,8 @@ async def responder(update: Update, context: ContextTypes.DEFAULT_TYPE):
     elif "bien y tú" in texto or "bien y tu" in texto:
         await update.message.reply_text("¡Muy bien, gracias!")
 
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, responder))
-
-print("Bot de Prueba 1 iniciado...")
-app.run_polling()
+if __name__ == "__main__":
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, responder))
+    print("Bot de Prueba 1 iniciado...")
+    app.run_polling()
