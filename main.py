cd237f4a34163428582400c06f9650e3039cfcda
erodemil <djpromto@gmail.com>
2025-10-19 15:10:53 -0400

    Fix indentation and clean up print statement

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ TOKEN = "8273893147:AAHI7WEUSgdgm9AVq5aMhgk24dKeJQ8KHM8"
 
 async def responder(update: Update, context: ContextTypes.DEFAULT_TYPE):
     texto = update.message.text.lower()
-
     if "hola" in texto:
         await update.message.reply_text("¡Hola amigo! ¿Cómo estás?")
     elif "bien y tú" in texto or "bien y tu" in texto:
@@ -14,5 +13,4 @@ async def responder(update: Update, context: ContextTypes.DEFAULT_TYPE):
 if __name__ == "__main__":
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, responder))
-    print("Bot de Prueba 1 iniciado...")
     app.run_polling()
